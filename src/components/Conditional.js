5af696a350475db04c0f1f9a46737a280d14f6e9
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2018-11-04 01:03:50 -0500

    update package

--- src/components/Conditional.js
+++ src/components/Conditional.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 const Conditional = props => !!props.if && props.children;
+
+Conditional.propTypes = {
+    if: PropTypes.bool.isRequired,
+    children: PropTypes.any
+};
+
 export default Conditional;
