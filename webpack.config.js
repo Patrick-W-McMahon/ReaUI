c6b9fd311af9f9ebc7e41bb3ddb5b3ef94987b35
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2021-02-13 18:54:34 -0700

    refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,45 @@
 var path = require('path');
-module.exports = {
-  entry: './src/index.js',
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = ['source-map'].map((devtool) => ({
+  entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "build"),
+    filename: 'reax-ui.js',
+    library: 'reax-ui'
   },
+  resolve: {
+    extensions: ['.es6', '.js', '.jsx'],
+    modules: ['node_modules']
+  },
+  externals: {},
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         include: path.resolve(__dirname, 'src'),
         exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
+        exclude: /(node_modules|bower_components|build)/,
       },
       {
         test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: "url-loader?limit=100000"
+        loader: "url-loader?limit=100000",
+        exclude: /(node_modules|bower_components|build)/,
       }
     ]
   },
   externals: {
     'react': 'commonjs react'
-  }
-};
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  devtool,
+  optimization: {
+    runtimeChunk: true,
+  },
+}));
